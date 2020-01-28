1a6535aa6cac8f34eda351e3479f3b1bbb40f13d
Nickolay <43444835+nmalinchik@users.noreply.github.com>
2020-01-28 14:14:06 +0300

    Update Operation.java

--- src/main/java/edu/myprojects/springbatchproject/model/Operation.java
+++ src/main/java/edu/myprojects/springbatchproject/model/Operation.java
@@ -4,7 +4,6 @@ import lombok.Data;
 
 import javax.persistence.Entity;
 import javax.persistence.Id;
-import java.util.Objects;
 
 @Entity
 public @Data class Operation {
@@ -14,20 +13,4 @@ public @Data class Operation {
     private String name;
     private String value;
     private String status;
-
-    @Override
-    public boolean equals(Object o) {
-        if (this == o) return true;
-        if (o == null || getClass() != o.getClass()) return false;
-        Operation operation = (Operation) o;
-        return id == operation.id &&
-                name.equals(operation.name) &&
-                value.equals(operation.value) &&
-                status.equals(operation.status);
-    }
-
-    @Override
-    public int hashCode() {
-        return Objects.hash(id, name, value, status);
-    }
 }
